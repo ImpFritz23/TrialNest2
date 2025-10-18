9893a7abc61fa491a6467b787d77c1ed311cd144
fritzimperial <fimperial@ncf.edu.ph>
2025-10-18 23:16:54 +0800

    tried NCF DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { PositionsModule } from './positions/positions.module';
 
-// @Module({
-//   imports: [DatabaseModule, UsersModule, AuthModule, PositionsModule],
-// })
+@Module({
+  imports: [DatabaseModule, UsersModule, AuthModule, PositionsModule],
+})
 
 // @Module({
 //   imports: [DatabaseModule],
